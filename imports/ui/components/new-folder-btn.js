cda749282da1393e9e3a0ed82fc9f0eeb025364f
Dimitris Klisiaris <dklisiaris@gmail.com>
2016-10-28 18:30:27 +0300

    Add new folder form.

--- imports/ui/components/new-folder-btn.js
+++ imports/ui/components/new-folder-btn.js
@@ -1,16 +1,72 @@
 import React from 'react';
 import {Meteor} from 'meteor/meteor';
+import PageClick from 'react-page-click';
 
-const _createNewFolder = () => {
-  if(Meteor.userId()){
-    Meteor.call("addFolder", Folders.defaultName(), null, true, function(err, folderId) {
-      Meteor.setTimeout(function(){ FlowRouter.go('/folders/' + folderId); }, 10);
+export class NewFolderBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormOpen: false
+    };
+
+    this._handleAddNewClick    = this._handleAddNewClick.bind(this);
+    this._handleCancelClick     = this._handleCancelClick.bind(this);
+    this._handleAddNewSubmit = this._handleAddNewSubmit.bind(this);
+  }
+
+  _handleAddNewClick(e) {
+    e.preventDefault();
+    this.setState({
+      isFormOpen: true
     });
-  } else {
-    Meteor.setTimeout(function(){ FlowRouter.go('/signin'); }, 10);
+  }
+
+  _handleCancelClick(e) {
+    e.preventDefault()
+    this.setState({
+      isFormOpen: false
+    });
+  }
+
+  _handleAddNewSubmit(e) {
+    e.preventDefault();
+
+    if(Meteor.userId()){
+      Meteor.call("addFolder", Folders.defaultName(), null, true, function(err, folderId) {
+        Meteor.setTimeout(function(){ FlowRouter.go('/folders/' + folderId); }, 10);
+      });
+    } else {
+      Meteor.setTimeout(function(){ FlowRouter.go('/signin'); }, 10);
+    }
+  }
+
+  _renderAddBtn() {
+    return (
+      <a onClick={this._handleAddNewClick} className="add-new header-btn" href="#"><i className="fa fa-plus"/></a>
+    );
+  }
+
+  _renderNewFolderForm() {
+    return (
+      <span>
+        <ul className="drop-down active">
+          <li>
+            <form onSubmit={this._handleAddNewSubmit}>
+              <h4>Add new folder</h4>
+              <input ref="title" type="text" required={true} placeholder="Folder name"/>
+              <button type="submit">Add folder</button> or <a onClick={this._handleCancelClick} href="#">cancel</a>
+            </form>
+          </li>
+        </ul>
+      </span>
+    );
+  }
+
+  render(){
+    return (
+      <span>
+        {this.state.isFormOpen ? this._renderNewFolderForm() : this._renderAddBtn()}
+      </span>
+    );
   }
 }
-
-export const NewFolderBtn = () => (
-  <a onClick={_createNewFolder} className="add-new header-btn" href="#"><i className="fa fa-plus"/></a>
-);
