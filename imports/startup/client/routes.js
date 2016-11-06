595db2222eb36a869dacd311409b012643c05284
Dimitris Klisiaris <dklisiaris@gmail.com>
2016-11-06 02:37:02 +0200

    Add auth to routes and redirect index route to folders.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, Redirect, browserHistory } from 'react-router';
 import { Meteor } from 'meteor/meteor';
 
 import {App} from '/imports/ui/layouts/app';
@@ -9,14 +9,24 @@ import {Signup} from '/imports/ui/views/signup';
 import {FoldersView} from '/imports/ui/views/folders';
 import {BookmarksView} from '/imports/ui/views/bookmarks';
 
+const authenticate = (nextState, replace) => {
+  if (!Meteor.loggingIn() && !Meteor.userId()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+};
+
 Meteor.startup( () => {
   render(
     <Router history={ browserHistory }>
+    <Redirect from="/" to="folders"/>
       <Route path="/" component={ App }>
         <Route name="login" path="/login" component={ Login } />
         <Route name="signup" path="/signup" component={ Signup } />
-        <Route name="folders" path="/folders" component={ FoldersView } />
-        <Route name="folder" path="/folders/:id" component={ BookmarksView } />
+        <Route name="folders" path="/folders" component={ FoldersView } onEnter={ authenticate } />
+        <Route name="folder" path="/folders/:id" component={ BookmarksView } onEnter={ authenticate } />
       </Route>
     </Router>,
     document.getElementById( 'react-root' )
