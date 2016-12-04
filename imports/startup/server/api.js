17c8a343f4f76069069157f4a137f4a5d55bf1a2
Dimitris Klisiaris <dklisiaris@gmail.com>
2016-12-04 12:15:39 +0200

    Import user api on startup

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -3,3 +3,6 @@ import '../../api/bookmarks/methods';
 
 import '../../api/folders/server/publications.js';
 import '../../api/folders/methods';
+
+import '../../api/users/server/publications.js';
+import '../../api/users/methods';
